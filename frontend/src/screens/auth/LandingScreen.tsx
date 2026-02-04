4cd2feefa0e31eb6a495bec3b805707e9a4862b0
Diya Kadakia <kadakia.d@northeastern.edu>
2026-02-04 14:46:14 -0500

    added buttons and text to landing screen

--- frontend/src/screens/auth/LandingScreen.tsx
+++ frontend/src/screens/auth/LandingScreen.tsx
@@ -1,7 +1,11 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Pressable } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { useAppNavigation } from '../../navigation/hooks';
 
 export function LandingScreen() {
+
+  const navigation = useAppNavigation();
+  
   return (
     <LinearGradient
       colors={['#FFFFFF', '#FFFFFF',  '#F5C76B','#F8B4A8',]}
@@ -11,12 +15,34 @@ export function LandingScreen() {
       end={{ x: 0.5, y: 1 }}
     >
       <View style={styles.content}>
+        <View style={styles.header}> 
         <Text style={styles.title}>R E M E T R A</Text>
         <View style={styles.taglineContainer}>
           <Text style={styles.taglineItalic}>peace of mind </Text>
           <Text style={styles.tagline}>begins here</Text>
         </View>
-      </View>
+      </View> 
+  
+
+      {/* Buttons */}
+      <View style={styles.buttonContainer}>
+        <Pressable 
+          style={styles.loginButton}
+          onPress={() => navigation.navigate("UserGoals")}
+        >
+          <Text style={styles.loginButtonText}>Log in</Text>
+        </Pressable>
+
+        <View style={styles.signupContainer}>
+            <Text style={styles.signupText}>New user? </Text>
+            <Pressable onPress={() => navigation.navigate("UserGoals")}>
+              <Text style={styles.signupLink}>Sign up</Text>
+            </Pressable>
+        </View>
+       </View>
+     </View>
+    
+
     </LinearGradient>
   );
 }
@@ -54,4 +80,39 @@ const styles = StyleSheet.create({
     fontFamily: 'serif',
     color: '#F8B4A8',
   },
-});
+  buttonContainer: {
+    width: '100%',
+    alignItems: 'center',
+    marginTop: 175,    
+  },
+  loginButton: {
+    borderWidth: 2,
+    borderColor: '#FFFFFF',
+    borderRadius: 8,
+    paddingVertical: 14,
+    paddingHorizontal: 80,
+    marginBottom: 16,
+  },
+  loginButtonText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  signupContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  signupText: {
+    color: '#FFFFFF',
+    fontSize: 14,
+  },
+  signupLink: {
+    color: '#FFFFFF',
+    fontSize: 14,
+    fontWeight: '600',
+  },
+  header: {
+  alignItems: 'center',
+},
+
+});   
