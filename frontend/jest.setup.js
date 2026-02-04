c8116e10117e73f387644ecd32eb4e06a4fcce95
Benjamin Welsh <welsh.beyond@gmail.com>
2026-02-04 00:52:39 -0500

    add jest setup and config + ts-node dependency

---
+++ frontend/jest.setup.js
@@ -0,0 +1,4 @@
+if (typeof global.structuredClone === 'undefined') {
+  global.structuredClone = (obj) => JSON.parse(JSON.stringify(obj));
+}
+global.__ExpoImportMetaRegistry = { register: () => {} };
