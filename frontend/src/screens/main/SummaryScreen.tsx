c59dfc235f7a94cb12690988278cf244c24346c6
TKDPenguin <tkdpenguin123@gmail.com>
2026-02-03 22:17:51 -0500

    Add simple text and buttons to summary screen

--- frontend/src/screens/main/SummaryScreen.tsx
+++ frontend/src/screens/main/SummaryScreen.tsx
@@ -1,5 +1,5 @@
 
-import { View, Button, StyleSheet } from 'react-native';
+import { View, Button, StyleSheet, Text } from 'react-native';
 import { useAppNavigation } from '../../navigation/hooks';
 
 export function SummaryScreen() {
@@ -7,9 +7,17 @@ export function SummaryScreen() {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>YOUR SUMMARY</Text>
       <Button title='To...' onPress={() => {navigation.navigate("Summary")}} />
+      <Text style={styles.text}> {GetPersonalizedIntro("Nicole", "stomache pain", "pizza", 5, 10)} </Text>
+      <Button title="VIEW ALL CORRELATIONS →" onPress={() => {}} />
+      <Button title="+ ADD DATA" onPress={() => {}} />
     </View>
   );
+}
+
+function GetPersonalizedIntro(name: string, symptom: string, food: string, numerator: number, denominator: number) {
+  return `${name}, in the last 7 days, your ${symptom} has correlated with eating ${food} ${numerator}/${denominator} times.`
 }
 
 const styles = StyleSheet.create({
@@ -21,5 +29,14 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 28,
     fontWeight: '600',
+    textAlign: 'center',
+    color: "#b2939b",
+  },
+  text: {
+    textAlign: 'center',
+    color: '#000000',
+  },
+  body: {
+    backgroundColor: "linear-gradient(to bottom, #ffffff, #fca450)"
   },
 });
