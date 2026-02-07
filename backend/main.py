80a0c68b227c36d3fc7b6d9bedf5fefadae3dbec
Keshav Goel <141261924+keshavgoel787@users.noreply.github.com>
2026-02-07 12:51:18 -0500

    add cors

--- backend/main.py
+++ backend/main.py
@@ -6,11 +6,21 @@ This module initializes the FastAPI application and registers all route handlers
 
 from fastapi import FastAPI
 from scalar_fastapi import get_scalar_api_reference
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Remetra API",
     description="Backend API for Remetra 😛",
     version="0.1.0",
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
