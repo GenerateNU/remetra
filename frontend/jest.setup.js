473f6f3bd137e6b3ca5fdfa2b0e79dfc912c3117
TKDPenguin <tkdpenguin123@gmail.com>
2026-02-16 10:48:10 -0500

    feat: added zustand store with tests

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -2,3 +2,6 @@ if (typeof global.structuredClone === 'undefined') {
   global.structuredClone = (obj) => JSON.parse(JSON.stringify(obj));
 }
 global.__ExpoImportMetaRegistry = { register: () => {} };
+jest.mock("@react-native-async-storage/async-storage", () =>
+  require("@react-native-async-storage/async-storage/jest/async-storage-mock")
+);
