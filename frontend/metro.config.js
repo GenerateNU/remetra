f96eee15d56dd62c3e7673cb5326905b057fc3b0
Benjamin Welsh <welsh.beyond@gmail.com>
2026-02-19 17:14:52 -0500

    revert metro config changes

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -3,6 +3,4 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.unstable_enablePackageExports = false;
-
 module.exports = withNativeWind(config, { input: './global.css' });
