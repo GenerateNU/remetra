09b6fc1266bd9916262a23fd750502d531e5a187
Diya Kadakia <kadakia.d@northeastern.edu>
2026-03-15 20:29:45 -0400

    food logs function

--- backend/analysis/data.py
+++ backend/analysis/data.py
@@ -2,16 +2,38 @@
 This is where you will define the functions for getting the food/symptom
 Log entry DTOsw from joining the food/symptom table with the food/symptom log table
 """
-from sqlalchemy.ext.asyncio import AsyncSession 
-from sqlalchemy import select
-
-from .models import FoodLogEntry, SymptomLogEntry
+from backend.models.food import Food
+from backend.models.food_log import FoodLog
 from backend.models.symptom import Symptom
 from backend.models.symptom_log import SymptomLog
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from .models import FoodLogEntry, SymptomLogEntry
 
 
-async def fetch_food_logs(db: Session, username: str) -> list[FoodLogEntry]: 
-  return
+async def fetch_food_logs(db: AsyncSession, username: str) -> list[FoodLogEntry]: 
+  stmt = (
+    select(FoodLog, Food)
+    .join(Food, FoodLog.food_id == Food.id)
+    .where(FoodLog.username == username)
+    .order_by(FoodLog.timestamp.asc())
+    
+  )
+
+  result = await db.execute(stmt)
+  rows = result.all()
+
+  return [
+    FoodLogEntry(
+      timestamp = food_log.timestamp,
+      ingredients = food.ingredients,
+      servings = food_log.quantity 
+
+    )
+    for food_log, food in rows 
+  ]
+
 
 async def fetch_symptom_logs(db: AsyncSession, username: str) -> list[SymptomLogEntry]: 
   stmt = (select(
