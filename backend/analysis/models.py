f672dc02933030c386ba9d55fb30de239ac18d4b
TKDPenguin <tkdpenguin123@gmail.com>
2026-03-16 23:24:57 -0400

    feat: add binary search algorithm to get all food logs within a variable time window of a symptom log

--- backend/analysis/models.py
+++ backend/analysis/models.py
@@ -1,14 +1,18 @@
-import datetime
 from dataclasses import dataclass
+from datetime import datetime
 
 
 @dataclass
 class FoodLogEntry:
     timestamp: datetime
-    ingredients: list[str] 
+    ingredients: list[str]
+
 
 @dataclass
 class SymptomLogEntry:
     timestamp: datetime
-    symptom_name: str       
-    intensity: int          
+    symptom_name: str
+    location: str
+    sensation: str
+    intensity: int
+    duration: int | None = None
