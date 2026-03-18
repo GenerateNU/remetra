c3ef0a4b1c6707142cc3a649b6eecece055aeea9
deekshita Madhalam <madhalam.d@northeastern.edu>
2026-03-17 20:56:18 -0400

    created model, ingesting of pdf, chunking, embedding, and running the code

---
+++ backend/models/KnowledgeChunk.py
@@ -0,0 +1,20 @@
+import uuid 
+
+from sqlalchemy import Column, String, Text
+from sqlalchemy.dialects.postgresql import UUID
+from pgvector.sqlalchemy import Vector
+from sqlalchemy.sql import func
+
+from database import Base
+
+
+class KnowledgeChunk(Base):
+
+    __tablename__ = 'chunk'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    content = Column(Text, nullable=False)
+    embedding = Column(Vector(1536), nullable=False)
+    source = Column(String, nullable=False)
+
+
