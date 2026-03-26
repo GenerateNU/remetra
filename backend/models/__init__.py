8d4fcd549febe1f1d1d1201c54ae721796130f35
Jerome Rodrigo <jeromerodrigo06@gmail.com>
2026-03-26 00:00:16 -0400

    Update __init__.py

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,1 +1,9 @@
-from models import food, food_log, knowledge_chunk, symptom, symptom_log, tag, user
+from models import (
+    food as food,
+    food_log as food_log,
+    knowledge_chunk as knowledge_chunk,
+    symptom as symptom,
+    symptom_log as symptom_log,
+    tag as tag,
+    user as user,
+)
